598e103a43fa95c73cd34c8adb3241af9ab90707
nmgwap@126.com <nmgwap@126.com>
2017-11-30 14:45:40 +0800

    增加接口数据

--- js/public.js
+++ js/public.js
@@ -1,5 +1,4 @@
 //页面跳转公用方法
-//只能跳转往上找同级的页面
 mui(".fatherbox").on('tap', '.publicjumppage', function() {
 	//获取id
 	var urlid = this.getAttribute("id");
@@ -11,7 +10,14 @@ mui(".fatherbox").on('tap', '.publicjumppage', function() {
 		plus.webview.show(urlid, "slide-in-right", 150);
 	})
 });
+
 //页面暂时还未开发完成提示
 mui(".notfatherbox").on('tap', '.publicjumppage', function() {
-	plus.nativeUI.toast( "功能正在开发!");
+	plus.nativeUI.toast("功能正在开发!");
+});
+
+//删除缓存功能
+mui(".notfatherbox").on('tap', '.clearCache', function() {
+	//清除缓存
+	plus.nativeUI.toast("清除缓存成功!");
 });
