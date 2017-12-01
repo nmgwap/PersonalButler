cef318e7934b52d2482195260634c13b82a26997
nmgwap@126.com <nmgwap@126.com>
2017-12-01 17:31:58 +0800

    关于功能实现

--- js/public.js
+++ js/public.js
@@ -11,13 +11,39 @@ mui(".fatherbox").on('tap', '.publicjumppage', function() {
 	})
 });
 
-//页面暂时还未开发完成提示
+//关于的那几个
 mui(".notfatherbox").on('tap', '.publicjumppage', function() {
-	plus.nativeUI.toast("功能正在开发!");
+	//获取id
+	var urlid = this.getAttribute("id");
+	//手机app操作
+	mui.plusReady(function() {
+		var all = plus.webview.all();
+		console.log(JSON.stringify(all))
+		//显示预加载页面
+		plus.webview.show(urlid, "slide-in-right", 150);
+	})
 });
 
 //删除缓存功能
 mui(".notfatherbox").on('tap', '.clearCache', function() {
 	//清除缓存
 	plus.nativeUI.toast("清除缓存成功!");
+});
+
+//检查更新功能
+mui(".notfatherbox").on('tap', '.update', function() {
+	//清除缓存
+	plus.nativeUI.toast("当前已经是最新版本!");
+});
+
+//分享功能
+mui(".notfatherbox").on('tap', '.shar', function() {
+	//清除缓存
+	plus.nativeUI.toast("正在开发!");
+});
+
+//页面暂时还未开发完成提示
+mui(".nofatherbox").on('tap', '.publicjumppage', function() {
+	//清除缓存
+	plus.nativeUI.toast("功能正在开发!");
 });
